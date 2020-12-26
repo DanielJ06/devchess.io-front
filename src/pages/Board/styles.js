56f4d906615ff3752dec7de977f949290c6c8626
danieljesus <daniel.j.rodrigues06@gmail.com>
2020-12-26 14:00:26 -0300

    Fix container position

--- src/pages/Board/styles.js
+++ src/pages/Board/styles.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: left;
+  justify-content: center;
   min-width: 1050px;
 `;
