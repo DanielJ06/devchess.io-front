e1439ac7a1d871b2269f356854bd1884c9492427
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-01-08 14:15:48 -0300

    Fix react router structure

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 
 import GlobalStyles from './styles/global';
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Board from './pages/Board';
 import Home from './pages/Home';
+import Waiting from './pages/Waiting';
+import BlackBoard from './pages/BlackBoard';
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <BrowserRouter>
-        <Route path="/" component={Home} />
-        <Route path="/game/:gameId" component={Board} exact />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/matchmaking/:gameId" component={Waiting} />
+          <Route path="/game/:gameId" component={Board} />
+          <Route path="/bgame/:gameId" component={BlackBoard} />
+        </Switch>
       </BrowserRouter>
     </>
   );
